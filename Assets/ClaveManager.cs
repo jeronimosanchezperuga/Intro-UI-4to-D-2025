fc52ef2ec800c9927f834e225a99a6b2d52ea193
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-05-05 10:39:29 -0300

    Ejercicio Password terminado

---
+++ Assets/ClaveManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ClaveManager : MonoBehaviour
+{
+    public TextMeshProUGUI txtResultado;
+    public TMP_InputField inputClave;
+    string claveCorrecta = "1234";
+    string claveIngresada;
+    string mensaje;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtResultado.text = "";
+    }
+
+    public void ChequearClave()
+    {
+        claveIngresada = inputClave.text;
+        if (claveIngresada == claveCorrecta)
+        {
+            mensaje = "Clave correcta";
+        }
+        else
+        {
+            mensaje = "Clave Incorrecta";
+        }
+
+        txtResultado.text = mensaje;
+    }  
+}
